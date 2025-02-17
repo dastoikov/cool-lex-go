24eb3133fa95093264447ec0c1744900233e6e2c
Dimitar A. Stoikov <dimitar@samldom.com>
2025-02-17 21:01:41 +0200

    fix: copyright is 2025 and not 2021-2024

--- simplemath/simplemath_test.go
+++ simplemath/simplemath_test.go
@@ -1,4 +1,4 @@
-// Copyright 2021-2024 The Cool-lex-Go Contributors, see the CONTRIBUTORS file.
+// Copyright 2025 The Cool-lex-Go Contributors, see the CONTRIBUTORS file.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
 // except in compliance with the License. You may obtain a copy of the License at
